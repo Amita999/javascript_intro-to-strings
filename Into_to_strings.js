c070ca4fb0aa25083f9aaf3e9d50ab81d4ed8d98
amita.jethani <amitajethani@gmail.com>
2019-07-24 18:16:55 +0530

    Introduction to strings

---
+++ Into_to_strings.js
@@ -0,0 +1,33 @@
+var myStr = "Hello";
+console.log(myStr);
+var myStr1 = "Sandra";
+console.log(myStr1);
+//escape characters
+var escapeString = "I have a \"new laptop\"";
+console.log(escapeString);
+//single quote to avoid escape character 
+var escapeString1 = 'I have a "new laptop"';
+console.log(escapeString1);
+// Using various characters
+// using \n
+var usingChar = "This is a new line\n\t\\Second Line \n Third Line";
+console.log(usingChar);
+
+//Finding the Length of the string
+var strLength = "Hello BAe";
+console.log(strLength);
+var len = strLength.length;
+console.log("length of string "+len);
+//Bracket notation
+var firstletter = strLength[0];
+console.log(firstletter);
+var secletter = strLength[1];
+console.log(secletter);
+var thirletter = strLength[2];
+console.log(thirletter);
+var fourtletter = strLength[3];
+console.log(fourtletter);
+
+
+
+
